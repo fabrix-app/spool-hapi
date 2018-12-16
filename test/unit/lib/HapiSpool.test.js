56c949934c69d6710122a7811dbb57a78cb7214f
Mould <PMould@eleadcrm.com>
2018-12-16 14:39:45 -0600

    add test for we.options - temporary remove before() call to fix unit tests

---
+++ test/unit/lib/HapiSpool.test.js
@@ -0,0 +1,39 @@
+'use strict'
+const lib = require('../../../dist/index')
+const assert = require('assert')
+const FabrixApp = require('@fabrix/fabrix').FabrixApp
+const HapiSpool = require('../../../dist/index').HapiSpool
+const RouterSpool = require('@fabrix/spool-router').RouterSpool
+
+describe('app.spools.HapiSpool', () => {
+  let app
+  describe.only('#configure', () => {
+    it('should validate that web.options is set', (done) => {
+      app = new FabrixApp({
+        pkg: {},
+        config: {
+          main: {
+            spools: [
+              RouterSpool
+            ]
+          },
+          web: {
+            options: {
+              routes: {
+                cors: true
+              }
+            }
+          }
+        },
+        api: {}
+      })
+      const spool = new HapiSpool(app, {})
+      spool.configure()
+      it('should set this.serverConfig.routes.cors to true', () => {
+        assert.ok(spool.serverConfig.routes.cors)
+      })
+
+      done();
+    })
+  });
+});
